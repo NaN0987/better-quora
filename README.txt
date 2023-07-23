020002ca6c37dc8fb545a9092aade93b3dc52e14
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2023-07-22 22:31:03 -0400

    Added comments button
    Ok I'm gonna spend the next 3-5 days learning Node.js and Puppeteer bye bye

--- README.txt
+++ README.txt
@@ -8,11 +8,12 @@ Current features:
         -Setting Quora to dark mode by default
 
     -Prevents "please sign in to continue" popup from appearing while browsing the site
+    -Adds a "comments" button to the question so you can read question details (if available)
 
 Planned features:
     -Question details
         -Search through the comments on a question, take all comments from the person who posted the question and/or the quora question details bot, and append them under the question header.
-        -Append a button under the question header that links to the question's comments (comments can be found under www.quora.com/question-name/comments)
+        -Make the comments button say how many comments there are (ex: 2 Comments)
     
     -Make something appear in the popup (not sure what to put there)
     -Add a settings page
@@ -20,4 +21,4 @@ Planned features:
         -Include an option to remove the AI response to questions
         -Include an option to remove "related questions" box in feed
     
-    -Make Quora Plus questions easier to read by removing the fade out (and look into getting around the Quora plus block).
+    -Make Quora Plus questions easier to read by removing the fade out (and look into getting around the Quora plus block by removing cookies).
--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,22 @@
+//querySelector constants
+//if the website changes and we have to chanage the queryselectors, these constants should make that easier
+const qs_dataNosnippet = 'span[data-nosnippet="true"]'
+
+const qs_relatedQuestionBox = '#mainContent > div:not([class]) > div[class="q-box qu-borderAll qu-borderRadius--small qu-borderColor--raised qu-boxShadow--small qu-mb--small qu-bg--raised"]'
+
+const qs_questionFeed = "#mainContent > div:not([class])"
+
+const qs_questionTitle = "#mainContent > div.q-box.qu-borderAll.qu-borderRadius--small.qu-borderColor--raised.qu-boxShadow--small.qu-bg--raised > div > div > span > span > div > div > div > span > span"
+
+const qs_signupWall = "#root > div > div.q-box > div:nth-child(2) > div"
+
+const qs_sideAds = "#root > div > div.q-box > div:nth-child(1) > div.q-box.puppeteer_test_question_main > div > div:nth-child(2) > div > div > div.q-sticky"
+
+const qs_bottomRelatedQuestions = "#mainContent > div.q-box.qu-mt--small > div.dom_annotate_related_questions"
 
 //Removing "promoted" posts
 function removeDataNosnippet(){
-  const elements = document.querySelectorAll('span[data-nosnippet="true"]') //Note: this also selects ads that are already hidden. fix later
+  const elements = document.querySelectorAll(qs_dataNosnippet) //Note: this also selects ads that are already hidden. fix later
   
   if (elements.length > 0){
     console.log("Data nosnippet: ", elements.length)
@@ -14,7 +29,7 @@ function removeDataNosnippet(){
 
 //Removing related question boxes
 function removeRelatedQuestions(){
-  const elements = document.querySelectorAll('#mainContent > div:not([class]) > div[class="q-box qu-borderAll qu-borderRadius--small qu-borderColor--raised qu-boxShadow--small qu-mb--small qu-bg--raised"]') //Note: this also selects related questions that are already hidden. fix later
+  const elements = document.querySelectorAll(qs_relatedQuestionBox) //Note: this also selects related questions that are already hidden. fix later
   
   if (elements.length > 0){
     console.log("Related questions: ", elements.length)
@@ -48,6 +63,18 @@ function reformatURL(url){
   return result
 }
 
+//Add "comments" button to post
+function addCommentButton(){
+  const questionTitle = document.querySelector(qs_questionTitle)
+
+  const htmlCode = '<hr/><a href="' + reformatURL(document.URL) + '/comments' + '"><h6 style="text-decoration: underline;">Comments</h6></a>'
+
+  const newElement = document.createElement('div')
+  newElement.innerHTML = htmlCode
+
+  questionTitle.insertAdjacentElement('afterend', newElement)
+}
+
 //callback for observer
 const cb_docChange = (mutationsList, observer) => {
   for (const mutation of mutationsList){
@@ -63,34 +90,34 @@ const cb_docChange = (mutationsList, observer) => {
 //Note to self: the current url of the DOM can be accessed with document.URL (returns a string and includes the "https://" part of the url)
 
 //Main section of webpage containing posts​
-const pageMain = document.querySelector("#mainContent > div:not([class])")
+const questionFeed = document.querySelector(qs_questionFeed)
 
 //Run code if main section of page exists
-if (pageMain){
-  console.log("Main page detected")
+if (questionFeed){
+  console.log("Question feed detected")
 
   //Send message to notify that a question is being viewed
   chrome.runtime.sendMessage({message: "fetchQuestionDetails"})
 
+  //add "comments" button under question title
+  addCommentButton()
+
   //Remove signup wall
-  const signupWall = document.querySelector("#root > div > div.q-box > div:nth-child(2) > div")
+  const signupWall = document.querySelector(qs_signupWall)
 
   if (signupWall){
     console.log("signup page detected")
     signupWall.remove()
-    //This is fucking stupid
-    //document.querySelector("#root > div > div.q-box > div:nth-child(1)").removeAttribute("filter")
-    //document.querySelector("body").removeAttribute("overflow")
-    document.querySelector("#root > div > div.q-box > div:nth-child(1)").style.filter = "none"
-    document.querySelector("body").style.overflow = "initial"
+    document.querySelector("#root > div > div.q-box > div:nth-child(1)").style.filter = "none" //remove blur
+    document.querySelector("body").style.overflow = "initial" //enable scrolling
   }
 
   //Remove ads that appear on the right side of the screen
-  const sideAd = document.querySelector("#root > div > div.q-box > div:nth-child(1) > div.q-box.puppeteer_test_question_main > div > div:nth-child(2) > div > div > div.q-sticky")
+  const sideAd = document.querySelector(qs_sideAds)
   sideAd?.remove()
 
   //Remove "related questions" at the bottom of the screen
-  const relatedBox = document.querySelector("#mainContent > div.q-box.qu-mt--small > div.dom_annotate_related_questions")?.parentElement
+  const relatedBox = document.querySelector(qs_bottomRelatedQuestions)?.parentElement
   relatedBox?.remove()
   removeRelatedQuestions()
 
@@ -98,5 +125,5 @@ if (pageMain){
   removeDataNosnippet()
 
   const observer = new MutationObserver(cb_docChange)
-  observer.observe(pageMain, {childList: true, subtree: true,})
+  observer.observe(questionFeed, {childList: true, subtree: true,})
 }
